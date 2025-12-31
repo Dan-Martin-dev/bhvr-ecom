125836c0e6e16b69218ddb6f52616c392f5f2cd0
Vare <dm.va@hotmail.com>
2025-12-31 20:19:54 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,24 @@
+import { db } from "@bhvr-ecom/db";
+import * as schema from "@bhvr-ecom/db/schema/auth";
+import { env } from "@bhvr-ecom/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
